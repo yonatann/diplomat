d765516a47e535b0c03c0e7c2c5a76bab963b4ed
Kien Nguyen Trung <trungkien2288@gmail.com>
2016-06-19 20:14:49 +0700

    Remove IO.puts

--- lib/diplomat/client.ex
+++ lib/diplomat/client.ex
@@ -23,11 +23,9 @@ defmodule Diplomat.Client do
     |> call("commit")
     |> case do
       {:ok, body} ->
-        IO.puts "The response body: #{inspect body}"
         decoded = Diplomat.Proto.CommitResponse.decode(body)
         {:ok, decoded}
       any ->
-        IO.puts "bad response: #{inspect any}"
         any
     end
   end
