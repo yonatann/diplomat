17d01e0ee9ef62a9d133eb660b8e2934b0614032
Phil Burrows <github@philburrows.com>
2016-07-20 15:59:39 -0500

    update the goth dependency

--- mix.exs
+++ mix.exs
@@ -16,11 +16,10 @@ defmodule Diplomat.Mixfile do
 
   defp deps do
     [
-      {:goth, "~> 0.1.1"},
+      {:goth, "~> 0.1.4"},
       {:exprotobuf, git: "https://github.com/bitwalker/exprotobuf"},
       {:httpoison, "~> 0.8.0"},
       {:poison, "~> 2.1"},
-      # {:chatterbox, github: "joedevivo/chatterbox"},
       {:bypass, "~> 0.1", only: :test},
       {:mix_test_watch, "~> 0.2.5", only: :dev},
       {:ex_doc, ">= 0.0.0", only: [:dev]},
