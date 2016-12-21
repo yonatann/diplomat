145b5dc4c01a091041c5a2263d265d3a446d2605
Phil Burrows <github@philburrows.com>
2016-12-20 21:57:57 -0600

    version bump 0.3.0 --> 0.3.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Diplomat.Mixfile do
 
   def project do
     [app: :diplomat,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: ">= 1.3.0",
      description: "A library for interacting with Google's Cloud Datastore",
      package: package,
