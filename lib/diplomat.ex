b7c455ba1b392211e709397486f65e2693775a27
Phil Burrows <github@philburrows.com>
2016-12-20 22:50:31 -0600

    more updates to the protobuf definitions
    currently, we still have one failing test

--- lib/diplomat.ex
+++ lib/diplomat.ex
@@ -7,6 +7,6 @@ defmodule Diplomat do
   similar, but not exactly like, SQL).
   """
   defmodule Proto do
-    use Protobuf, from: Path.expand("datastore_v1beta3.proto", __DIR__), doc: false
+    use Protobuf, from: Path.expand("datastore_v1.proto", __DIR__), doc: false
   end
 end
