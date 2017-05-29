67e0b28d319c41743847ef718320fb5669e5de22
Chad Jablonski <chad@dinocore.net>
2017-05-29 13:32:50 -0700

    Add typespecs to `Value`

--- lib/diplomat/value.ex
+++ lib/diplomat/value.ex
@@ -7,8 +7,13 @@ defmodule Diplomat.Value do
   alias Diplomat.Proto.LatLng, as: PbLatLng
   alias Diplomat.{Entity, Key}
 
+  @type t :: %__MODULE__{
+    value: any,
+  }
+
   defstruct value: nil
 
+  @spec new(any) :: t
   def new(val=%{__struct__: struct}) when struct in [Diplomat.Entity, Diplomat.Key, Diplomat.Value],
     do: %__MODULE__{value: val}
   def new(val=%{__struct__: _struct}),
@@ -20,6 +25,7 @@ defmodule Diplomat.Value do
   def new(val),
     do: %__MODULE__{value: val}
 
+  @spec from_proto(PbVal.t) :: t
   def from_proto(%PbVal{value_type: {:boolean_value, val}}) when is_boolean(val),
     do: new(val)
   def from_proto(%PbVal{value_type: {:integer_value, val}}) when is_integer(val),
@@ -35,7 +41,7 @@ defmodule Diplomat.Value do
   def from_proto(%PbVal{value_type: {:entity_value, %PbEntity{} = val}}),
     do: val |> Diplomat.Entity.from_proto |> new
   def from_proto(%PbVal{value_type: {:array_value, %PbArray{} = val}}) do
-    %__MODULE__{value: Enum.map(val.values, &Diplomat.Value.from_proto/1)}
+    %__MODULE__{value: Enum.map(val.values, &from_proto/1)}
   end
   def from_proto(%PbVal{value_type: {:timestamp_value, %PbTimestamp{} = val}}) do
     val.seconds * 1_000_000_000 + (val.nanos || 0)
@@ -48,6 +54,7 @@ defmodule Diplomat.Value do
     do: new(nil)
 
   # convert to protocol buffer struct
+  @spec proto(any) :: PbVal.t
   def proto(nil),
     do: PbVal.new(value_type: {:null_value, :NULL_VALUE})
   def proto(%__MODULE__{value: val}),
