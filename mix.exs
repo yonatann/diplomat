96ca56fa0d0410b0402ab518dfbcc7beb4533ab4
Tan Jay Jun <tan@jayjun.com>
2017-07-26 18:01:19 +0800

    Support Poison 3.1

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Diplomat.Mixfile do
       {:goth, "~> 0.3"},
       {:exprotobuf, "~> 1.2"},
       {:httpoison, "~> 0.11"},
-      {:poison, "~> 2.1"},
+      {:poison, "~> 2.2 or ~> 3.1"},
       {:bypass, "~> 0.1", only: :test},
       {:mix_test_watch, "~> 0.2.5", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev},
