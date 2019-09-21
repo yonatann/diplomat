815ea799d09136df5438a17f407deb7b74d38dd4
yonatannaor <yonatan.naor@gmail.com>
2019-09-21 18:59:10 +0300

    update httppoision

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Diplomat.Mixfile do
       {:credo, "~> 0.8", only: [:dev, :test]},
       {:goth, "~> 1.0"},
       {:exprotobuf, "~> 1.2"},
-      {:httpoison, "~> 0.11"},
+      {:httpoison, "~> 1.0"},
       {:poison, "~> 2.2 or ~> 3.1"},
       {:bypass, "~> 0.8", only: :test},
       {:mix_test_watch, "~> 0.4", only: :dev},
